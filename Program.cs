e21582665de942f2ef17b2f59327c3d4a29bd16b
pranjalofficial <pranjalofficial1996@gmail.com>
2019-10-28 14:31:52 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace newApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("New Application");
         }
     }
 }
